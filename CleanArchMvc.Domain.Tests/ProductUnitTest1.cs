b15f818d0de09ca0a2ae616bfe1e1cd6415c4ddc
Oruam Motta <62652819+OruamC@users.noreply.github.com>
2023-02-25 16:23:35 -0300

    Add project files.

---
+++ CleanArchMvc.Domain.Tests/ProductUnitTest1.cs
@@ -0,0 +1,99 @@
+﻿using CleanArchMvc.Domain.Entities;
+using FluentAssertions;
+using System;
+using Xunit;
+
+
+namespace CleanArchMvc.Domain.Tests
+{
+    public class ProductUnitTest1
+    {
+        [Fact(DisplayName = "Create Product With Valid Parameter Valid State")]
+        public void CreateProduct_WithValidParameters_ResultObjectValidState()
+        {
+            Action action = () => new Product(1, "Product Name", "Procut Description", 9.99m, 99, "product image");
+            action.Should()
+                .NotThrow<CleanArchMvc.Domain.Validation.DomainExceptionValidation>();
+        }
+
+        [Fact(DisplayName = "Create Product With Negative Id Domain Exception Invalid Id")]
+        public void CreateProduct_NegativeIdValue_DomainExceptionInvalidId()
+        {
+            Action action = () => new Product(-1, "Product Name", "Procut Description", 9.99m, 99, "product image");
+            action.Should()
+                .Throw<CleanArchMvc.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid Id value.");
+        }
+
+        [Fact(DisplayName = "Create Product With Short Name Domain Exception Short Name")]
+        public void CreateProduct_ShortNameValue_DomainExceptionShortName()
+        {
+            Action action = () => new Product(1, "Pr", "Procut Description", 9.99m, 99, "product image");
+            action.Should()
+                .Throw<CleanArchMvc.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid name, too short. Minimum 3 characteres.");
+        }
+
+        [Fact(DisplayName = "Create Product With Null Name Domain Exception Invalid Name")]
+        public void CreateProduct_NullNameValue_DomainExceptionInvalidName()
+        {
+            Action action = () => new Product(1, null, "Procut Description", 9.99m, 99, "product image");
+            action.Should()
+                .Throw<CleanArchMvc.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid name. Name is required.");
+        }
+
+        [Fact(DisplayName = "Create Product With Short Description Domain Exception Short Description")]
+        public void CreateProduct_ShortDescriptionValue_DomainExceptionShortDescription()
+        {
+            Action action = () => new Product(1, "Product Name", "Proc", 9.99m, 99, "product image");
+            action.Should()
+                .Throw<CleanArchMvc.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid description, too short. Minimum 5 characteres.");
+        }
+
+        [Fact(DisplayName = "Create Product With Negative Price Domain Exception Invalid Price")]
+        public void CreateProduct_NegativePriceValue_DomainExceptionInvalidPrice()
+        {
+            Action action = () => new Product(1, "Product Name", "Procut Description", -9.99m, 99, "product image");
+            action.Should()
+                .Throw<CleanArchMvc.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid price value.");
+        }
+
+        [Fact(DisplayName = "Create Product With Negative Stock Domain Exception Invalid Stock")]
+        public void CreateProduct_NegativeStockValue_DomainExceptionInvalidStock()
+        {
+            Action action = () => new Product(1, "Product Name", "Procut Description", 9.99m, -99, "product image");
+            action.Should()
+                .Throw<CleanArchMvc.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid stock value.");
+        }
+
+        [Fact(DisplayName = "Create Product With Null Image Valid State")]
+        public void CreateProduct_NullImageValue_ReturnObjectValidState()
+        {
+            Action action = () => new Product(1, "Product Name", "Procut Description", 9.99m, 99, null);
+            action.Should()
+                .NotThrow<CleanArchMvc.Domain.Validation.DomainExceptionValidation>();
+        }
+
+        [Fact(DisplayName = "Create Product With Null Image Valid State")]
+        public void CreateProduct_NullImageValue_NoNullReferenceException()
+        {
+            Action action = () => new Product(1, "Product Name", "Procut Description", 9.99m, 99, null);
+            action.Should()
+                .NotThrow<NullReferenceException>();
+        }
+
+        [Fact(DisplayName = "Create Product With Long Image Name Domain Exception Long Image Name")]
+        public void CreateProduct_LongImageValue_DomainExceptionLongImageName()
+        {
+            Action action = () => new Product(1, "Product Name", "Procut Description", 9.99m, 99, 
+                "product image toooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooog");
+            action.Should()
+                .Throw<CleanArchMvc.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid image name, too long. Maximum 250 characteres.");
+        }
+    }
+}
